1811ce54a8b4d5a63ebf1e773ec33ff83b69c4af
harrypahwa97 <harshpahwa97@gmail.com>
2020-10-17 20:23:55 +0530

    SumOfPrimesTillN

---
+++ Harsh Pawha's Submissions/SumPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SumPrime {
+	public static void main(String harsh[]){
+		Scanner s= new Scanner(System.in);
+		int N = s.nextInt();
+		if(N<3){
+			if(N<2)
+				System.out.print("Sum of Prime Numbers till "+N+" = "+"0");
+			else if(N==2)
+				System.out.print("Sum of Prime Numbers till "+N+" = "+"2");
+			return;
+		}
+		int PrimeArray[] = new int[1000001];
+		PrimeArray[2]=1;
+		for(int i=3;i<PrimeArray.length;i+=2){
+			PrimeArray[i]=1;
+		}
+		for(int i=3;i<PrimeArray.length;i+=2){
+			for(int j=2*i;j<PrimeArray.length;j+=i){
+				PrimeArray[j]=0;
+			}
+		}
+		int SumOfPrimeTillN=2;
+		for(int i=3;i<=N;i+=2){
+			if(PrimeArray[i]==1)
+				SumOfPrimeTillN+=i;
+		}
+		System.out.print("Sum of Prime Numbers till "+N+" = "+SumOfPrimeTillN);
+	}
+}
