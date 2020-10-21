2570662a24ac82d730ee62bde4ee486389d56e71
harrypahwa97 <harshpahwa97@gmail.com>
2020-10-21 19:43:56 +0530

    Added Solution to Q8

---
+++ Q8/harrypahwa97/Run.md
@@ -0,0 +1,11 @@
+# Run.md 
+
+## How to compile
+```
+javac SumPrime.java
+```
+
+## How to Run
+```
+java SumPrime
+```
---
+++ Q8/harrypahwa97/SumPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SumPrime {
+	public static void main(String harsh[]){
+		Scanner s= new Scanner(System.in);
+		int N = s.nextInt();
+		if(N<3){
+			if(N<2)
+				System.out.print("Sum of Prime Numbers till "+N+" = "+"0");
+			else if(N==2)
+				System.out.print("Sum of Prime Numbers till "+N+" = "+"2");
+			return;
+		}
+		int PrimeArray[] = new int[N+1];
+		PrimeArray[2]=1;
+		for(int i=3;i<PrimeArray.length;i+=2){
+			PrimeArray[i]=1;
+		}
+		for(int i=3;i<PrimeArray.length;i+=2){
+			for(int j=2*i;j<PrimeArray.length;j+=i){
+				PrimeArray[j]=0;
+			}
+		}
+		int SumOfPrimeTillN=2;
+		for(int i=3;i<=PrimeArray.length;i+=2){
+			if(PrimeArray[i]==1)
+				SumOfPrimeTillN+=i;
+		}
+		System.out.print("Sum of Prime Numbers till "+N+" = "+SumOfPrimeTillN);
+	}
+}
