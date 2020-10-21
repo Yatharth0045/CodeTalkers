63501f3b06e2ac8574d0b8629d40a37fbedd2517
harrypahwa97 <harshpahwa97@gmail.com>
2020-10-21 19:57:06 +0530

    Added Solution to Q8

--- Q8/harrypahwa97/SumPrime.java
+++ Q8/harrypahwa97/SumPrime.java
@@ -12,10 +12,10 @@ public class SumPrime {
 		}
 		int PrimeArray[] = new int[N+1];
 		PrimeArray[2]=1;
-		for(int i=3;i<PrimeArray.length;i+=2){
+		for(int i=3;i<=PrimeArray.length;i+=2){
 			PrimeArray[i]=1;
 		}
-		for(int i=3;i<PrimeArray.length;i+=2){
+		for(int i=3;i<=PrimeArray.length;i+=2){
 			for(int j=2*i;j<PrimeArray.length;j+=i){
 				PrimeArray[j]=0;
 			}
