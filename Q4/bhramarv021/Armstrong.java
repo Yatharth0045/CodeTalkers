8f18f87a2f446a091ccfd742d50f042b0625dd16
bhramarv021 <bhramarv021@gmail.com>
2020-10-31 03:00:25 +0530

    Added solution to Q4

---
+++ Q4/bhramarv021/Armstrong.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Armstrong
+{
+	public static void main(String[] args) {
+	int n=0,i=0,sum=0,lastNumber=0,temp=0;
+	Scanner sc = new Scanner(System.in);
+	n = sc.nextInt();
+	temp = n;
+	while(temp>0){
+		lastNumber = temp%10;
+		sum = lastNumber*lastNumber*lastNumber+sum;
+		temp /= 10;
+	}
+	if(n==sum)
+		System.out.println("YES");
+	else
+		System.out.println("NO");	
+	}
+}
---
+++ Q4/bhramarv021/run.md
@@ -0,0 +1,11 @@
+# Run.md 
+
+## How to compile
+```
+javac Solution.java
+```
+
+## How to Run
+```
+java Solution
+```
