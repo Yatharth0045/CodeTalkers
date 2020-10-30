6abb9c9b63bb1e12d8efca331fc43aeb51891d9f
bhramarv021 <bhramarv021@gmail.com>
2020-10-31 03:11:40 +0530

    Added solution to Q1

---
+++ Q1/bhramarv021/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Palindrome{
+	public static void main(String[] args) {
+		int n = 0;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int reverse = findRev(n);
+		if(n==reverse)
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+	}
+
+	public static int findRev(int x){
+		int temp,r = 0;
+		while(x>0){
+			temp = x%10;
+			r = r*10+temp;
+			x /= 10;
+		}
+		return r;
+	}
+}
