ac76b6a36e02c52b49af3f0dc82ba70e2e7621d9
Yatharth Sharma <yatharth0045@gmail.com>
2020-10-30 05:42:53 +0530

    Added Question 3

---
+++ Q3/yatharth0045/BuildPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class BuildPattern{
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        pattern(scan.nextInt());
+    }
+
+    static void pattern(int n){
+        
+        for(int k = 1;k <= n ; k++){
+            for(int i = 1 ; i <= n ; i++){
+                for(int j = 1; j <= (n+1-k) ; j++){
+                    System.out.print(((n+1)-i)+" ");
+                }
+            }
+            System.out.println();
+        } 
+    }
+}
