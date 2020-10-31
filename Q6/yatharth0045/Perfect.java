2a6c4c35e24cb81947312544efd8829d00bc62b3
Yatharth Sharma <yatharth0045@gmail.com>
2020-10-31 21:17:46 +0530

    Added solution to 6

---
+++ Q6/yatharth0045/Perfect.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Perfect {
+
+    public static void main (String args[]) {
+        Scanner scan = new Scanner(System.in);
+        if(isPerfect(scan.nextInt())){
+            System.out.println("Number is Perfect");
+        }else{
+            System.out.println("Number is not Perfect");
+        }
+    }
+
+    static boolean isPerfect(int number){
+        int sum = 0;
+        for(int i=1;i<=number/2+1;i++){
+            if(number%i==0){
+                sum+=i;
+            }
+        }
+        if(number==sum){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
